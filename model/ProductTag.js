d777adb860bda902407b0cbfd32e26fd6b28ad92
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 17:33:34 -0400

    created ProductTag model in ProductTag.js

--- model/ProductTag.js
+++ model/ProductTag.js
@@ -1,2 +1,40 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    // define model options
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "productTag",
+  }
+);
+
+module.exports = ProductTag;
