a58894358570d4f73f3ec14d680980c6bb4a6b8a
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 21:29:51 -0400

    added code to getCategoryById function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -16,15 +16,29 @@ const getAllCategories = async (req, res) => {
   }
 };
 
-// Function that gets one category by id
+// This is an async function called getCategoryById
+// It is called when a user makes a GET request to /api/categories/:id
 const getCategoryById = async (req, res) => {
   try {
-    // code here
+    // The id from req.params is stored in a variable called id
+    const id = req.params.id;
+    // The findByPk method is a sequelize method that will find one piece of data from the Category Model
+    // where the id is equal to what is specified in the req.params
+    const categoryData = await Category.findByPk(id);
+    // If no data is found, a 404 status is sent to the client with a json message
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id" });
+      return;
+    }
+    // If the data is found, it is sent as a json to the client
+    res.json(categoryData);
   } catch (error) {
+    // If there is an error, the error is logged and a 500 status is sent to the client with a json message
     console.log({ error });
     res.status(500).json({ error: "Failed to get category by id" });
   }
 };
+
 // Function that creates a new category
 const createCategory = async (req, res) => {
   try {
