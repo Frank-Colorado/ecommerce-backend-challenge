f1404446bee62497626cc26486c04b34833cc46a
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:22:30 -0400

    added code to getAllProducts function

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,16 @@
 const { Product } = require("../models");
 
-// Function that gets all products
+// This is an async function called getAllProducts
+// It is called when a user makes a GET request to /api/products
 const getAllProducts = async (req, res) => {
   try {
-    // code here
+    // The findAll method is a sequelize method that will find all of the data from the Product Model
+    // It is then stored in the productData variable
+    const productData = await Product.findAll();
+    // The data is then sent as a json to the client
+    res.json(productData);
   } catch (error) {
+    // If there is an error, the error is logged and a 500 status is sent to the client with a json message
     console.log({ error });
     res.status(500).json({ error: "Failed to get all products" });
   }
