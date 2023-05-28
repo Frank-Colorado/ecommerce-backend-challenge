df2f6e891317022806bf225c1556d538014ae462
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:32:03 -0400

    added CRUD routes in product-routes

--- routes/api-routes/product-routes/index.js
+++ routes/api-routes/product-routes/index.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../../../controllers/productController");
+
+// Routing for the path /api/products
+router
+  .route("/")
+  // Get all products
+  .get(getAllProducts)
+  // Create a new product
+  .post(createProduct);
+
+// Routing for the path /api/products/:id
+router
+  .route("/:id")
+  // Get a single product by its `id`
+  .get(getProductById)
+  // Update a product by its `id`
+  .put(updateProduct)
+  // Delete a product by its `id`
+  .delete(deleteProduct);
 
 module.exports = router;
