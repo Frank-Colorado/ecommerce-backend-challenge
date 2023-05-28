88c98df17f355b208387b358d976f7b5595667e1
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:40:13 -0400

    added CRUD routes for tag-routes

--- routes/api-routes/tag-routes/index.js
+++ routes/api-routes/tag-routes/index.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
+const {
+  getAllTags,
+  getTagById,
+  createTag,
+  updateTag,
+  deleteTag,
+} = require("../../../controllers/tagController");
+
+// Routing for the path /api/tags
+router
+  .route("/")
+  // Get all tags
+  .get(getAllTags)
+  // Create a new tag
+  .post(createTag);
+
+// Routing for the path /api/tags/:id
+router
+  .route("/:id")
+  // Get a single tag by its `id`
+  .get(getTagById)
+  // Update a tag by its `id`
+  .put(updateTag)
+  // Delete a tag by its `id`
+  .delete(deleteTag);
 
 module.exports = router;
