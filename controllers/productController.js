6eff8a90a2a03cb48387c277f0a9fdffd2ecd4c9
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 20:16:40 -0400

    created CRUD functions in productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,59 @@
 const { Product } = require("../models");
 
 // Function that gets all products
+const getAllProducts = async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error: "Failed to get all products" });
+  }
+};
 
 // Function that gets one product by id
+const getProductById = async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error: "Failed to get product by id" });
+  }
+};
 
 // Function that creates a new product
+const createProduct = async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error: "Failed to create product" });
+  }
+};
 
 // Function that updates a product by id
+const updateProduct = async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error: "Failed to update product" });
+  }
+};
 
 // Function that deletes a product by id
+const deleteProduct = async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    console.log({ error });
+    res.status(500).json({ error: "Failed to delete product" });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
