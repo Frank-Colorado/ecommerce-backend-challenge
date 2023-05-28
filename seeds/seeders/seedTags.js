ba14058771be581a0d7749fe3be069d56125734f
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:44:55 -0400

    fixed bug in seedTags.js file

--- seeds/seeders/seedTags.js
+++ seeds/seeders/seedTags.js
@@ -3,7 +3,7 @@ const { Tag } = require("../../models");
 const seedTags = async () => {
   try {
     // An array of tag names is created
-    const tags = [New, Sale, Trending, Popular, Seasonal, Limited];
+    const tags = ["New", "Sale", "Trending", "Popular", "Seasonal", "Limited"];
     // The tags array is mapped through
     // For each tag in the array, the tag_name column is set to the tag name
     // The result is an array of objects which is passed to the bulkCreate method to create data in the Tag Model
