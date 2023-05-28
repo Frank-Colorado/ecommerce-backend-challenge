f5e159804b5abf131aafa1c772b0fa8a2fd83903
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:35:36 -0400

    added code to getTagById function

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -16,11 +16,24 @@ const getAllTags = async (req, res) => {
   }
 };
 
-// Function that gets one tag by id
+// This is an async function called getTagById
+// It is called when a user makes a GET request to /api/tags/:id
 const getTagById = async (req, res) => {
   try {
-    // code here
+    // The id from req.params is stored in a variable called id
+    const { id } = req.params;
+    // The findByPk method is a sequelize method that will find one piece of data from the Tag Model
+    // where the id is equal to what is specified in the req.params
+    const tagData = await Tag.findByPk(id);
+    // If no data is found, a 404 status is sent to the client with a json message
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id" });
+      return;
+    }
+    // If the data is found, it is sent as a json to the client
+    res.json(tagData);
   } catch (error) {
+    // If there is an error, the error is logged and a 500 status is sent to the client with a json message
     console.log({ error });
     res.status(500).json({ error: "Failed to get tag by id" });
   }
