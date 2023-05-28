5826d145eca3b7fd45f2141fb575c49b052d819d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:47:30 -0400

    fixed bug in seedCategories.js file

--- seeds/seeders/seedCategories.js
+++ seeds/seeders/seedCategories.js
@@ -3,7 +3,14 @@ const { Category } = require("../../models");
 const seedCategories = async () => {
   try {
     // An array of category names is created
-    const categories = [Electronics, Clothing, Home, Food, Toys, Tools];
+    const categories = [
+      "Electronics",
+      "Clothing",
+      "Home",
+      "Food",
+      "Toys",
+      "Tools",
+    ];
     // The categories array is mapped through
     // For each category in the array, the category_name column is set to the category name
     // The result is an array of objects which is passed to the bulkCreate method to create data in the Category Model
