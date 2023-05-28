2b04002a3a50dd1ebb22ce449e029bff4aa4744b
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 21:32:47 -0400

    added code to createCategory function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -39,11 +39,18 @@ const getCategoryById = async (req, res) => {
   }
 };
 
-// Function that creates a new category
+// This is an async function called createCategory
+// It is called when a user makes a POST request to /api/categories
 const createCategory = async (req, res) => {
   try {
-    // code here
+    // req.body is destructured
+    const { category_name } = req.body;
+    // The create method is a sequelize method that will create a new piece of data in the Category Model
+    const categoryData = await Category.create({ category_name });
+    // The new data is sent as a json to the client
+    res.json(categoryData);
   } catch (error) {
+    // If there is an error, the error is logged and a 500 status is sent to the client with a json message
     console.log({ error });
     res.status(500).json({ error: "Failed to create category" });
   }
