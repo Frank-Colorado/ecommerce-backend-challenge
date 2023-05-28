6f8e2bf32be3c4b968c2d5dc86154e724584061c
Frank-Colorado <chawnkiee@gmail.com>
2023-05-27 22:53:55 -0400

    fixed bug in seedProduct.js file

--- seeds/seeders/seedProducts.js
+++ seeds/seeders/seedProducts.js
@@ -1,14 +1,18 @@
-const { Product } = require("../../models");
+const { Product, Category } = require("../../models");
 const { faker } = require("@faker-js/faker");
 
 const seedProducts = async () => {
   try {
+    // The findAll method is used to get all categories that were created in the previous step
+    // The result is an array
+    // We need the length of the array to use in the faker.number.int method
+    const allCategories = await Category.findAll();
     // A product object is created with the faker library for the Product Model
     const productObj = {
       product_name: faker.commerce.productName(),
       price: faker.commerce.price(),
       stock: faker.number.int(),
-      category_id: faker.number.int({ min: 1, max: categories.length }),
+      category_id: faker.number.int({ min: 1, max: allCategories.length }),
     };
     // The product object is passed to the faker.helpers.multiple method to create an array of 50 products
     const products = faker.helpers.multiple(productObj, { count: 50 });
